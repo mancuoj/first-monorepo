feb17d0c3aead80cb9e72b3ab1c9497da800e8fd
mancuoj <1337381768@qq.com>
2023-06-03 13:10:25 +0800

    update

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
 /* eslint-disable no-console */
-import { isEven } from '@mancuoj/is-even'
-import { isOdd } from '@mancuoj/is-odd'
-
-console.log(isEven(1))
-console.log(isOdd(1))
+export { isEven } from '@mancuoj/is-even'
+export { isOdd } from '@mancuoj/is-odd'
